160810196675cdca685b915150ebf19602a02a89
Abdelghafour <ghafourragg@gmail.com>
2022-03-10 23:44:50 +0100

    _

---
+++ src/components/Core.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Core = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit
+      laudantium corrupti, magni vero eveniet. Asperiores vel, reprehenderit
+      sequi id, obcaecati culpa, ullam veniam labore temporibus unde aliquid
+      recusandae distinctio.
+    </div>
+  );
+};
+
+export default Core;
