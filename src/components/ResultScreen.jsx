8b8e2b7a99853d1e319b75c4fbb2984706444b8c
Abdelghafour <ghafourragg@gmail.com>
2022-03-14 16:27:35 +0100

    added the word breaking into result screen

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ResultScreen = ({ onPut }) => {
-  return <div className="result-screen">{onPut}</div>;
+  return (
+    <div className="result-screen">
+      <div className="prev-operand">1234</div>
+      <div className="curr-operand">{onPut}</div>
+    </div>
+  );
 };
 
 export default ResultScreen;
